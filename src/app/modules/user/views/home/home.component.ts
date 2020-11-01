aa1acf21b2781e6ab94e66089b46e5904bdbb924
Asmaa Sherif ElKholy <aselkholy461997@gmail.com>
2020-11-01 21:26:26 +0200

    Updated User Home

--- src/app/modules/user/views/home/home.component.ts
+++ src/app/modules/user/views/home/home.component.ts
@@ -1,15 +1,18 @@
+import { User } from './../../../../models/user.model';
+import { UserService } from './../../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.currentUser = this.userService.currentUser;
   }
-
 }
